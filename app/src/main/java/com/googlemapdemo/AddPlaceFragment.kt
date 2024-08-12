73738853f95cd7ba339a88bc844ac00fc33d9e88
Uthoai Marma <uthoaimarma597@gmail.com>
2024-08-12 18:49:18 +0600

    calender date picker

--- app/src/main/java/com/googlemapdemo/AddPlaceFragment.kt
+++ app/src/main/java/com/googlemapdemo/AddPlaceFragment.kt
@@ -1,27 +1,44 @@
 package com.googlemapdemo
 
+import android.app.DatePickerDialog
+import android.icu.util.Calendar
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.appbar.MaterialToolbar
 import com.googlemapdemo.databinding.FragmentAddPlaceBinding
+import java.text.SimpleDateFormat
+import java.util.Locale
 
-class AddPlaceFragment : Fragment() {
+class AddPlaceFragment : Fragment(), View.OnClickListener {
     private lateinit var binding: FragmentAddPlaceBinding
+    private var calendar = Calendar.getInstance()
+    private lateinit var dateSetListener: DatePickerDialog.OnDateSetListener
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentAddPlaceBinding.inflate(inflater, container, false)
 
+        dateSetListener =
+            DatePickerDialog.OnDateSetListener { datePicker, year, month, day_of_month ->
+                calendar.set(Calendar.YEAR, year)
+                calendar.set(Calendar.MONTH, month)
+                calendar.set(Calendar.DAY_OF_MONTH, day_of_month)
+                updateDateInView()
+            }
+
+        binding.etDate.setOnClickListener(this)
+
         setListener()
 
         return binding.root
     }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -45,4 +62,26 @@ class AddPlaceFragment : Fragment() {
     private fun setListener() {
         //
     }
+
+    override fun onClick(view: View?) {
+        when (view?.id) {
+            R.id.et_date -> {
+                DatePickerDialog(
+                    requireContext(),
+                    dateSetListener,
+                    calendar.get(Calendar.YEAR),
+                    calendar.get(Calendar.MONTH),
+                    calendar.get(Calendar.DAY_OF_MONTH)
+                ).show()
+
+            }
+        }
+    }
+
+    private fun updateDateInView() {
+        val myFormat = "dd/MM/yyyy"
+        val sdf = SimpleDateFormat(myFormat, Locale.getDefault())
+        binding.etDate.setText(sdf.format(calendar.time).toString())
+    }
+
 }
